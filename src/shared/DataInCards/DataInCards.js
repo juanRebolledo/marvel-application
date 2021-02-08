86daf11a620d89ee340cec8439d019a0a14bc2e9
Jose Juan Rebolledo Kamey <juanrekamey1234@gmail.com>
2021-02-07 19:07:38 -0600

    asd

--- src/shared/DataInCards/DataInCards.js
+++ src/shared/DataInCards/DataInCards.js
@@ -4,15 +4,14 @@ import Card from 'shared/Card/Card'
 import Loader from 'shared/Loader/Loader'
 
 const DataInCards = ({data, to}) => (
-  data ? data.filter((item) => !item.thumbnail.path.includes("image_not_available")).map(item => (
+  data ? data.length > 0 ? data.filter((item) => !item.thumbnail.path.includes("image_not_available")).map(item => (
     <Card 
       key={item.id} 
       to={`/${to}/${item.id}`} 
       thumbnail={item.thumbnail} 
       title={item.title || item.name} 
-    />
-  )
-  ) : <Loader />
+    />)
+  ) : <p>No hay información, verifica tu busqueda</p> : <Loader />
 )
 
 export default React.memo(DataInCards)
